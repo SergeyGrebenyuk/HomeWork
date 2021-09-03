0fbfcb80783dc09bf53f6b5c6d6817c75a562c32
SergeyGrebenyuk <serggr.grebenyuk@gmail.com>
2021-09-03 16:48:22 +0300

    ...

--- src/main/java/ru/geekbrains/jca/home3/homeFree.java
+++ src/main/java/ru/geekbrains/jca/home3/homeFree.java
@@ -8,9 +8,10 @@ import java.util.Scanner;
 
 public class homeFree {
     public static void main(String[] args) {
-      //  fillSquareArrayDiagonally(10);          //Ответ на 1.п задания
-      //  guessNumber();
-        guessWord();
+
+      fillSquareArrayDiagonally(10);          //Ответ на 1.п задания
+      guessNumber();                               //Ответ на 2.п задания
+      guessWord();                                 //Ответ на 3.п задания
 
     }
     //метод для распечатки массива
@@ -39,15 +40,15 @@ public class homeFree {
     }
 
 
-    /*Написать программу, которая загадывает случайное число от 0 до 9 и пользователю
+    /* 2п. Написать программу, которая загадывает случайное число от 0 до 9 и пользователю
      дается 3 попытки угадать это число. При каждой попытке компьютер должен сообщить,
      больше ли указанное пользователем число, чем загаданное, или меньше. После победы
      или проигрыша выводится запрос –
      «Повторить игру еще раз? 1 – да / 0 – нет»(1 – повторить, 0 – нет).*/
     public static void guessNumber() {
         int answer=0;
-        int hiddenNumber=0;
-        int repeat = 1;
+        int hiddenNumber;
+        int repeat;
         Scanner sc = new Scanner(System.in);
         do {
                 System.out.println("Угадайте число от 0 до 9. Вам дается 3 попытки");
@@ -72,10 +73,9 @@ public class homeFree {
             repeat = sc.nextInt();
         } while (repeat==1);
         System.out.println("GAME OVER");
-        sc.close();
     }
 
-    /* **Создать массив из слов String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
+    /* 3п. **Создать массив из слов String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado",
        "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut",
        "olive", "pea", "peanut", "pear", "pepper", "pineapple","pumpkin", "potato"}.
         При запуске программы компьютер загадывает слово, запрашивает ответ у пользователя, сравнивает его с загаданным
@@ -84,32 +84,50 @@ public class homeFree {
         пользователь не мог узнать длину слова) Для сравнения двух слов посимвольно можно пользоваться:
         String str = "apple"; char a = str.charAt(0); - метод, вернет char, который стоит в слове str на первой позиции
         Играем до тех пор, пока игрок не отгадает слово. Используем только маленькие буквы.*/
+
     public static void guessWord() {
-        int i = 0;
-        Scanner sc = new Scanner(System.in);
-        String[] guessedLetters = new String[15];
+        int i;
+        int j=0;
+        Scanner wc = new Scanner(System.in);
         Random rnd= new Random();
+        String answerUser;
         String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry",
                 "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut",
                 "pear", "pepper", "pineapple","pumpkin", "potato"};
-        for (i = 0; i < 15; i++) {                          //Заполняем проверочное слово символом #
-            guessedLetters[i] = "#";
+
+        char[] hint= new char[15];
+        for (i = 0; i < 15; i++) {                          //Заполняем подсказку символом #
+            hint[i] = '#';
         }
-        int randomIndex = rnd.nextInt(words.length);    //загадываем слово
-        System.out.println(words[randomIndex]);         //выводим загаданное слово
-        System.out.println(Arrays.toString(guessedLetters));
-        String answer = sc.nextLine();
-        for (i = 0; i < words.length; i++) {
-            if (words[i].equals(answer)) {
-                System.out.println("Угадал");
+
+        int randomIndex = rnd.nextInt(words.length);         //определяем случайным образом индекс загаданного слова
+        String wordGuess=words[randomIndex];                 //загаданное слово
+
+        System.out.println(wordGuess);                      //печатаем загаданное слово
+
+        System.out.println("Я загадал слово из следующего списка {\n" +Arrays.toString(words) +"\n" + "} попробуй отгадай его");
+        System.out.println("Подумай хорошо и напиши свой вариант: ");
+
+        while (true) {
+            answerUser = wc.nextLine();                         //Ждем ответ от игрока
+            if (answerUser.toLowerCase().equals(wordGuess)) {
+                System.out.println("Вы угадали!!!");
                 break;
             }
-
-
-
+            for (i = 0; i < answerUser.length() && i < wordGuess.length(); i++) {    // определяем совпадающие буквы
+                if (answerUser.charAt(i) == wordGuess.charAt(i)) {
+                    hint[i] = answerUser.charAt(i);
+                    j=j+1; //счетчик угаданных букв
+                }
+            }
+            if (j!=0) {
+                System.out.println("У тебя почти получилось, ты угадал несколько букв правильно.\n"
+                        + "Подумай еще и у тебя все получится:");
+                System.out.println(Arrays.toString(hint));
+            }
+            System.out.println("Твой ответ не верен. Сосредоточься! Это так просто!!!");
+            }
         }
-
-
     }
 
-}
+
